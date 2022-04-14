2e27fffd5a4959fd86b1e51a4e4d08c430f816a0
Lebez Quentin <103516033+Qlebez@users.noreply.github.com>
2022-04-14 22:18:24 +0200

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       </div>
       <div className='desc'>
         <h3>
-          I'm m an 18 year old french student at the IUT of Gradignan.
+          I'm am a 18 year old french student at the IUT of Gradignan.
           I'm currently in first year of university bachelors of computer science.
         </h3>
       </div>
