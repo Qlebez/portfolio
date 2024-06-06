bfab738070ea6705c99045a831e14c9e4d4a2fb0
Lebez Quentin <quentin.lebez@etu.u-bordeaux.fr>
2024-06-06 21:35:19 +0200

    wip - revamp

---
+++ src/components/GeneralSkill.vue
@@ -0,0 +1,261 @@
+<template>
+  <div class="generalskill">
+    <h1>
+      I've learned a lot of different things throughout my short journey in IT :
+    </h1>
+    <div class="skill-Header">
+      <h2>Frontend</h2>
+      <q-separator />
+    </div>
+    <div class="frontend skillsList">
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg"
+        />
+        <p>VueJS</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/quasar/quasar-original.svg"
+        />
+        <p>Quasar</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        />
+        <p>ReactJS</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nuxtjs/nuxtjs-original.svg"
+        />
+        <p>NuxtJS</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
+        />
+        <p>ViteJS</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+        />
+        <p>JavaScript</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+        />
+        <p>TypeScript</p>
+      </div>
+    </div>
+    <div class="skill-Header">
+      <h2>Backend</h2>
+      <q-separator />
+    </div>
+    <div class="backend skillsList">
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg"
+        />
+        <p>C#</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg"
+        />
+
+        <p>.NET Core</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+        />
+        <p>Python</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"
+        />
+        <p>Java</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg"
+        />
+        <p>PHP</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/symfony/symfony-original.svg"
+        />
+        <p>Symfony</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/laravel/laravel-original.svg"
+        />
+        <p>Laravel</p>
+      </div>
+    </div>
+    <div class="skill-Header">
+      <h2>Database</h2>
+      <q-separator />
+    </div>
+    <div class="database skillsList">
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"
+        />
+        <p>MySQL</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original-wordmark.svg"
+        />
+
+        <p>Microsoft SQL Server</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
+        />
+        <p>MongoDB</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"
+        />
+        <p>PostgreSQL</p>
+      </div>
+    </div>
+    <div class="skill-Header">
+      <h2>Others</h2>
+      <q-separator />
+    </div>
+    <div class="others skillsList">
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
+        />
+        <p>Git</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg"
+        />
+        <p>Bash</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
+        />
+        <p>GitHub</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original.svg"
+        />
+        <p>GitLab</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original.svg"
+        />
+        <p>Azure</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg"
+        />
+        <p>Docker</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg"
+        />
+        <p>Linux</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/archlinux/archlinux-original.svg"
+        />
+        <p>Arch</p>
+      </div>
+    </div>
+    <div class="skill-Header">
+      <h2>Currently learning</h2>
+      <q-separator />
+    </div>
+    <div class="learning skillsList">
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rust/rust-original.svg"
+        />
+        <p>Rust</p>
+      </div>
+      <div class="skill">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
+        />
+        <p>NodeJs</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+img {
+  width: 100px;
+  height: 100px;
+}
+
+div {
+  background-color: #313d5a;
+  color: white;
+}
+
+.skill-Header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  font-size: 2em;
+  font-weight: bold;
+  text-align: center;
+  padding-top: 5%;
+}
+
+h2 {
+  padding-top: 5%;
+  font-size: 4em;
+  text-align: center;
+  font-weight: bold;
+}
+
+.q-separator {
+  width: 75%;
+  margin: 0 auto;
+  margin-bottom: 5%;
+  background-color: white;
+}
+
+.skillsList {
+  display: flex;
+  justify-content: center;
+}
+
+.skill {
+  padding: 2%;
+  text-align: center;
+}
+.skill p {
+  font-size: 1.5em;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <PresentationComponent />
+    <GeneralSkill />
+  </div>
+</template>
+<script setup lang="ts">
+import PresentationComponent from 'src/components/PresentationComponent.vue';
+import GeneralSkill from 'src/components/GeneralSkill.vue';
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+body {
+  font-family: 'Poppins', sans-serif;
+  margin: 0;
+  padding: 0;
+}
+</style>
