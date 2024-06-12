78279d9a013a8dae3a2a4451bf8923074e4de356
Lebez Quentin <quentin.lebez@etu.u-bordeaux.fr>
2024-06-12 19:29:32 +0200

    revamp

---
+++ src/components/ProjectShowcase.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>Project Showcase</h1>
+        <h4>
+            Here are some of the projects I have worked on during my studies and my apprenticeship
+        </h4>
+        <div>
+            <h2>Minishell</h2>
+            <h4>
+                As the name suggest it, Minishell is a minimal shell that I have created with a team of 3 people. It is a project from the 2nd year of my computer science degree.
+                We had to recreate a shell with some basic functionalities like the builtins echo, cd, pwd, export, unset and env. We also had to handle the PATH variable and the errors.
+                We used the C language.
+            </h4>
+        </div>
+        <div>
+            <h2>Management Webapp</h2>
+            <h4>
+                During my internship in Germany, I worked with a team of 3 mechanical engineers and in partnership with Premium Aerotec, we created a Webapp
+                to manage there milling machines. Through real-time data, we were able to check the status of the machines, the number of pieces produced, 
+                the number of pieces remaining, the time remaining, etc. The engineering team provide me the technical expertise and I was in charge of the development with a team of 2 others french students
+                We used the Laravel framework with PHP and the React library with JavaScript and TailwindCSS for the design.
+            </h4>
+        </div>
+        <div>
+            <h2>Project 3</h2>
+            <h4>
+                Description of the project
+            </h4>
+        </div>
+        <div>
+            <h2>Project 4</h2>
+            <h4>
+                Description of the project
+            </h4>
+        </div>
+        <div>
+            <h2>Project 5</h2>
+            <h4>
+                Description of the project
+            </h4>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="scss">
+div {
+    background-color: #694D75;
+}
+</style>
